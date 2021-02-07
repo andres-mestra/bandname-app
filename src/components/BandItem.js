fa055e77da139dc21750ea42a15f2dd27bba1664
Andres Mestra <andresmestra11a@gmail.com>
2021-02-07 00:04:33 -0500

    Refactorización BandItem usando SocketContext

--- src/components/BandItem.js
+++ src/components/BandItem.js
@@ -1,21 +1,33 @@
 import * as React from 'react'
+import { SocketContext } from '../context/SocketContext';
 
-export const BandItem = ({ 
-  band, 
-  handleNameChange, 
-  handleOnBlur, 
-  votar,
-  deleteBand
-}) => {
+export const BandItem = ({ id, name, votes }) => {
 
-  const { id, name, votes } = band
+  const { socket } = React.useContext(SocketContext);
+  const [value, setValue] = React.useState(name)
+
+  const votar = () => socket.emit('votar-banda', id)
+
+  const handleDeleteBand = () => socket.emit('delete-banda', id)
+
+  
+  const handleNameChange = ({ target }) => {
+    const newName = target.value;
+
+    if( newName.trim().length > 0 ){
+      setValue(newName);
+    }
+  }
+ 
+  //Se dispara cuando se pierde el foco
+  const handleOnBlur = () => socket.emit('change-name-banda', {id, name: value })
 
   return (
     <tr>
       <td>
         <button 
           className="btn btn-primary"
-          onClick={() => votar( id )}
+          onClick={ votar }
         >
           +1
         </button>
@@ -24,16 +36,16 @@ export const BandItem = ({
         <input
           className="form-control"
           name="name"
-          value={ name }
-          onChange={ ( event ) => handleNameChange( event, id) }
-          onBlur={() => handleOnBlur(id, name)}
+          value={ value }
+          onChange={ handleNameChange }
+          onBlur={ handleOnBlur }
         />
       </td>
       <td><h3>{ votes }</h3></td>
       <td>
         <button
           className="btn btn-danger"
-          onClick={() => deleteBand(id) }
+          onClick={ handleDeleteBand }
         >
           Borrar
         </button>
